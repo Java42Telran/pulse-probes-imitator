8978737822b79d4efff9593ff7a330cc4302dbfd
Yuragranovsky <yuragranovsky@gmail.com>
2022-06-22 21:03:57 +0300

    final code for PulseImitator

--- src/main/java/telran/monitoring/PulseProbesImitatorApplication.java
+++ src/main/java/telran/monitoring/PulseProbesImitatorApplication.java
@@ -14,7 +14,6 @@ import telran.monitoring.dto.PulseProbe;
 
 @SpringBootApplication
 public class PulseProbesImitatorApplication {
-private static final long TIMEOUT = 30000;
 @Value("${app.amount.patients: 10}")
 	int nPatients;
 @Value("${app.min.pulse.value: 40}")
@@ -23,18 +22,19 @@ int minPulseValue;
 int maxPulseValue;
 @Value("${app.jump.probability: 10}")
 int jumpProb;
-@Value("${app.jump.multiple: 0.8}")
+@Value("${app.jump.multiple: 0.6}")
 double jumpMultiple;
-@Value("{app.delta.increase.probability: 80}")
+@Value("${app.value.multiple: 0.1}")
+double valueMultiple;
+@Value("${app.delta.increase.probability: 80}")
 int deltaIncreaseProb;
-int sequenceNumber = 1;
+
 ThreadLocalRandom tlr = ThreadLocalRandom.current();
-HashMap<Long, Integer> patientsPulseValue = new HashMap<>();
+HashMap<Long, Integer> patientsPulseValues = new HashMap<>();
 
 	public static void main(String[] args) throws InterruptedException {
-		ConfigurableApplicationContext ctx = SpringApplication.run(PulseProbesImitatorApplication.class, args);
-		Thread.sleep(TIMEOUT);
-		ctx.close();
+		 SpringApplication.run(PulseProbesImitatorApplication.class, args);
+		
 	}
 	@Bean
 	Supplier<PulseProbe> pulseProbesSupplier() {
@@ -44,11 +44,24 @@ HashMap<Long, Integer> patientsPulseValue = new HashMap<>();
 		
 		long patientId = tlr.nextInt(1, nPatients + 1);
 		int value = getValue(patientId);
-		return new PulseProbe(sequenceNumber++, patientId  , value);
+		patientsPulseValues.put(patientId, value);
+		return new PulseProbe( patientId  , value);
 	}
 	private int getValue(long patientId) {
-		// TODO Auto-generated method stub
-		return 0;
+		Integer value = patientsPulseValues.get(patientId);
+		return value == null ? tlr.nextInt(minPulseValue, maxPulseValue) : getNewValue(value);
+	}
+	private int getNewValue(Integer value) {
+		int delta = (int) (isChance(jumpProb) ? value * jumpMultiple :
+			value * tlr.nextDouble(0, valueMultiple));
+		if (value + delta > maxPulseValue || !isChance(deltaIncreaseProb)) {
+			delta = -delta;
+		}
+		return value + delta;
+	}
+	private boolean isChance(int prob) {
+		
+		return tlr.nextInt(0, 100) < prob;
 	}
 
 }
